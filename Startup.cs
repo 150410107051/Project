7910851bcf9255f001ed7de3549e5b1e184b9bba
suraj more <29055479+150410107051@users.noreply.github.com>
2019-08-02 15:27:54 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeReferalSystem.Startup))]
+namespace EmployeeReferalSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
